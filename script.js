7eaadbab5646eab827087f3157977ae0daecb63c
inimimii <rabbanymimii@gmail.com>
2025-07-11 08:59:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+AOS.init();
+
+document.getElementById("mobile-menu-toggle").addEventListener("click", function () {
+  const menu = document.getElementById("mobile-menu");
+  menu.classList.toggle("hidden");
+});
