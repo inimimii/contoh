d03794027f9205bd99db839d1222d6b67b19bc0a
inimimii <rabbanymimii@gmail.com>
2025-07-11 09:12:31 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-AOS.init();
-
-document.getElementById("mobile-menu-toggle").addEventListener("click", function () {
-  const menu = document.getElementById("mobile-menu");
-  menu.classList.toggle("hidden");
-});
